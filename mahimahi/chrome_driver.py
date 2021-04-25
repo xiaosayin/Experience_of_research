4d68988c55cebe2fa335e82808a2b858569ba68c
xiaosayin <50323395+xiaosayin@users.noreply.github.com>
2021-04-25 12:25:48 +0800

    Add files via upload

--- mahimahi/chrome_driver.py
+++ mahimahi/chrome_driver.py
@@ -4,7 +4,9 @@ from selenium.webdriver.chrome.options import Options
 import time
 
 PROXY = "172.0.0.1:8889"
-LOG_FILE = 'bitrate.log'
+timeout_LOG_FILE = 'timeout.log'
+bitrate_LOG_FILE = 'bitrate.log'
+ssim_LOG_FILE = 'ssim_db.log'
 chrome_user_dir = '/home/yinwenpei/data_save/chrome_data'
 os.system("rm -rf " + chrome_user_dir)
 os.system("mkdir " + chrome_user_dir)
@@ -15,8 +17,8 @@ options.add_argument('--user-data-dir=' + chrome_user_dir)
 options.add_argument('--ignore-certificate-errors')
 driver = webdriver.Chrome('/home/yinwenpei/data_save/chromedriver', chrome_options=options)
 
-#driver.get("http://100.64.0.1:8080")
-driver.get("http://127.0.0.1:8080")
+driver.get("http://100.64.0.1:8080")
+#driver.get("http://127.0.0.1:8080")
 
 log_in_button = driver.find_element_by_xpath("/html/body/div/div/div[3]/a[2]")
 log_in_button.click()
@@ -31,21 +33,43 @@ log_in_button2 = driver.find_element_by_xpath("/html/body/div/form/div[4]/button
 log_in_button2.click()
 last_bit_rate = 0.0
 bitrate_list = []
+last_ssim_db = 0.0
+ssim_db_list = []
 # get real bitrate of video
 #os.system("gedit bitrate.log")
 while(True):
     bit_rate = driver.find_element_by_xpath('//*[@id="video-bitrate"]')
+    ssim_db = driver.find_element_by_xpath('//*[@id="video-ssim"]')
+    error_xpath = driver.find_element_by_xpath('//*[@id="player-error-list"]')
     try:
         real_bit_rate = float(bit_rate.get_attribute("innerText"))
+        real_ssim_db = float(ssim_db.get_attribute("innerText"))
+        # inspect error
+        error_info = error_xpath.get_attribute("innerText")
     except:
         continue
+    if(error_info):
+        print(error_info)
+        driver.refresh()
+        with open(timeout_LOG_FILE, mode='a') as f3:
+            f3.writelines(error_info)
+            f3.writelines('\n')
+
     if(last_bit_rate != real_bit_rate):
         bitrate_list.append(real_bit_rate)
         last_bit_rate = real_bit_rate
         print(real_bit_rate)
-        with open(LOG_FILE,mode = 'a') as f:
+        with open(bitrate_LOG_FILE,mode = 'a') as f:
             f.writelines(str(real_bit_rate))
             f.writelines('\n')
+    if(last_ssim_db != real_ssim_db):
+        ssim_db_list.append(real_ssim_db)
+        last_ssim_db = real_ssim_db
+        with open(ssim_LOG_FILE, mode='a') as f2:
+            f2.writelines(str(real_ssim_db))
+            f2.writelines('\n')
+
+
 
 
 # qianqianzi
